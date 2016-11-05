deb8870360f4e037181c60042f569998e9a1764a
Skov0 <mathias@skovdev.net>
2016-11-05 17:20:19 +0100

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimpleNetMonitor")]
-[assembly: AssemblyDescription("Monitor network stability")]
+[assembly: AssemblyDescription("Monitor network connectivity")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SimpleNetMonitor")]
